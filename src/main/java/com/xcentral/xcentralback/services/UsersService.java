b71538f1889c528c05f659c2fb4dbcd255a4104f
Briceson <royzone4@gmail.com>
2024-09-08 14:51:15 -0400

    password encoder and login can read hashed passwords

--- src/main/java/com/xcentral/xcentralback/services/UsersService.java
+++ src/main/java/com/xcentral/xcentralback/services/UsersService.java
@@ -7,7 +7,7 @@ import com.xcentral.xcentralback.repos.UserRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-// import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -18,12 +18,12 @@ public class UsersService {
     @Autowired
     UserRepo userRepo;
 
-    // @Autowired
-    // private PasswordEncoder passwordEncoder;
+     @Autowired
+     private PasswordEncoder passwordEncoder;
 
     public String addNewUser(User user) {
         System.out.println("The method did get called, yo");
-        //user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
         return "New user added successfully!";
     }
