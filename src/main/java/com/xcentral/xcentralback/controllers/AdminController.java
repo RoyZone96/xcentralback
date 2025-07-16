8708501b89a6913e03201f23c4159440c862be0e
Briceson <royzone4@gmail.com>
2025-07-16 13:21:20 -0400

    worked on the admin control

--- src/main/java/com/xcentral/xcentralback/controllers/AdminController.java
+++ src/main/java/com/xcentral/xcentralback/controllers/AdminController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin")
+@CrossOrigin(origins = "http://localhost:3000")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
     @Autowired
